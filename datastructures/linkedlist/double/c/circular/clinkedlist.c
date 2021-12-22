d33ef2fa26e18aa0c5b812c8a942b587200a3bc9
Anish Sinha <anishsinha0128@gmail.com>
2021-12-22 14:23:28 -0500

    pushing to the end of a circular linked list works in C

--- datastructures/linkedlist/double/c/circular/clinkedlist.c
+++ datastructures/linkedlist/double/c/circular/clinkedlist.c
@@ -1,6 +1,15 @@
+#include "clinkedlist.h"
 #include <stdio.h>
 
 int main() {
-  printf("here");
+  CList *list1 = new_clist();
+  list1 = list1->push_tail(list1, 1);
+  printf("here\n");
+  printf("%d\n", list1->tail->val);
+  list1 = list1->push_tail(list1, 2);
+  printf("%d\n", list1->tail->val);
+  list1 = list1->push_tail(list1, 3);
+  printf("%d\n", list1->tail->val);
+
   return 0;
 }
--- datastructures/linkedlist/double/c/circular/clinkedlist.h
+++ datastructures/linkedlist/double/c/circular/clinkedlist.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,7 +13,43 @@ typedef struct CListNode {
 typedef struct CList {
   unsigned int length;
   CListNode *tail;
+  CListNode *current;
+  CListNode *(*_create_node)(int);
   void (*print)(const struct CList *);
+  bool (*is_empty)(const struct CList *);
+  struct CList *(*push_start)(struct CList *, int);
+  struct CList *(*push_tail)(struct CList *, int);
 } CList;
+
+bool is_empty(const CList *self) { return self->length == 0; }
+
+CListNode *_create_node(int val) {
+  CListNode *new_node = malloc(sizeof(CListNode));
+  new_node->next = NULL;
+  new_node->val = val;
+  return new_node;
+}
+
+CList *push_tail(CList *self, int val) {
+  CListNode *new_node = self->_create_node(val);
+  if (self->is_empty(self)) {
+    self->tail = new_node;
+    new_node->next = self->tail;
+  } else {
+    CListNode *t = self->tail;
+    t->next = new_node;
+    new_node->next = t;
+    self->tail = new_node;
+  }
+  return self;
+}
+
+CList *new_clist() {
+  CList *new_list = malloc(sizeof(CList));
+  new_list->is_empty = is_empty;
+  new_list->_create_node = _create_node;
+  new_list->push_tail = push_tail;
+  return new_list;
+}
 
 #endif
